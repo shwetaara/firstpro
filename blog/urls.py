5cef517c0647916abf7c061ababd554d07b96d98
shwetaara <shwetaarakeri17@yahoo.com>
2017-02-04 18:00:51 +0530

    added view and template for blog as well as css for the site

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.Post_list,name='Post_list'),
+    url(r'^Post/(?P<pk>\d+)/$' ,views.Post_detail,name='Post_detail'),
+    url(r'^Post/new/$',views.Post_new),
+    url(r'^Post/(?P<pk>\d+)/edit/$',views.Post_edit,name='Post_edit'),
+    url(r'^drafts/$',views.Post_draft_list, name='Post_draft_list'),
+    url(r'^Post/(?P<pk>\d+)/publish/$' ,views.Post_publish, name='Post_publish'),
+    url(r'^Post/(?P<pk>\d+)/remove/$', views.Post_remove, name='Post_remove'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import redirect
@@ -11,5 +11,54 @@ def Post_list(request):
 
 
 
+def Post_detail(request,pk):
+    Postc= get_object_or_404(Post, pk=pk)
+    Post.objects.get(pk=pk)
+    return render(request,'blog/Post_detail.html',{'Postc':Postc})
 
-# Create your views here.
+
+def Post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+                        if form.is_valid():
+                            Post = form.save(commit=False)
+                            Post.author = request.user
+                            Post.save()
+                            return redirect('Post_detail', pk=Post.pk)
+                        else:
+                            form = PostForm()
+                            return render(request, 'blog/Post_edit.html',{'form':form})
+
+
+def Post_edit(request, pk):
+    Post = get_object_or_404(Post,pk=pk)
+    if request.method == "POST":
+        form = PostFrom(request,POST,instance=Post)
+        if form.is valid():
+            Post = form.save(commit=False)
+            Post.author = request.user
+            Post.save()
+            return redirect('Post_detail', pk=Post.pk)
+        else:
+            form = PostForm(instance=Post)
+            return render(request,'blog/Post_edit.html',{'form'})
+
+
+def Post_draft_list(request):
+    Posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request,'blog/Post_draft_list.html',{'Posts':Posts})
+
+
+def Post_publish(request, pk):
+    Post = get_object_or_404(Post, pk=pk)
+    Post.publiush()
+    return redirect('Post_detail', pk=Post.pk)
+
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+
+def Post_remove(request, pk):
+    Post = get_object_or_404(Post, pk=pk)
+    Post.delete()
+    return redirect('Post_list')
